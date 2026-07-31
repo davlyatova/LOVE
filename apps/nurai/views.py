4860b3a408201d2364da9a08f91a3ddae21b7c3e
davlyatova <taty.dav@icloud.com>
2026-07-31 20:21:27 +0600

    Add POST method handling for apply form

--- apps/nurai/views.py
+++ apps/nurai/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student  
-
 def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        comment = request.POST.get('comment')
+        
+        print(f"Новая заявка! Имя: {name}, Тел: {phone}, Email: {email}, Коммент: {comment}")
+
+        return redirect('index') 
+
     students = Student.objects.all()
-    context = {
-        'students': students,
-    }
-       
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', {'students': students})
