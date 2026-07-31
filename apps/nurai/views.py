8aa75d1276613ecbafd491bab8c977539da651a4
davlyatova <taty.dav@icloud.com>
2026-07-31 20:09:14 +0600

    Connect CSS styles and render dynamic student data

--- apps/nurai/views.py
+++ apps/nurai/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from .models import Student
-
+from .models import Student  
 
 def index(request):
     students = Student.objects.all()
-    return render(request, 'index.html', locals())
+    context = {
+        'students': students,
+    }
+       
+    return render(request, 'index.html', context)
